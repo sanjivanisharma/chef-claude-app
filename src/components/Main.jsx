4973fe93785a05d46486c161b4f798edaca897fe
Sanjivani Sharma <sanjivanisharma01@gmail.com>
2025-07-23 14:21:44 +0530

    Code changes

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,18 @@
+import { useState } from "react"
+
 export default function Main() {
+    const [ingredients, setIngredients] = useState(["tomato"])
+    console.log(ingredients)
+
+    const ingredientsElementsList = ingredients.map((ingredient) => (
+        <li key={ingredient}>{ingredient}</li>
+    ))
 
     function handleSubmit(event) {
         event.preventDefault()
+        const formData = new FormData(event.currentTarget)
+        const newIngredient = formData.get("ingredient")
+        setIngredients((prevIngredient) => [...prevIngredient, newIngredient])
     }
 
     return (
@@ -11,10 +22,13 @@ export default function Main() {
                 type="text" 
                 aria-label="Add ingredient"
                 placeholder="e.g. oregano"
-                name="ingedient"
+                name="ingredient"
                 />
                 <button>Add ingredient</button>
             </form>
+            <ul>
+                {ingredientsElementsList}
+            </ul>
         </main>
     )
 }
